9429613f0349c3acd682d79e6d2e9bfb27b15042
Juan Pablo Ubelhart <juanpabloubelhart@hotmail.com>
2021-09-02 02:54:02 -0300

    VERSION 0.15: ARREGLOS VARIOS

--- js/script.js
+++ js/script.js
@@ -1,34 +1,38 @@
-/* let numeroUno = parseInt(prompt('Escribe un numero: '));
+let numeroUno = parseInt(prompt('Escribe un numero: '));
 let operador = prompt('Escribe un operador: + - * /');
 let numeroDos = parseInt(prompt('Escribe otro numero:'));
 let resultado;
 
 function calcular(numeroUno, operador, numeroDos) {
 
-  if (isNaN(numeroUno || numeroDos)) {
-    alert('No escribiste un numero');
-  }
+  if (isNaN(numeroUno) || isNaN(numeroDos)) {
 
-  switch (operador) {
-    case '+':
-      resultado = numeroUno + numeroDos;
-      break;
-    case '-':
-      resultado = numeroUno - numeroDos;
-      break;
-    case '*':
-      resultado = numeroUno * numeroDos;
-      break;
-    case '/':
-      resultado = numeroUno / numeroDos;
-      break;
-    default:
-      alert('No es un operador correcto');
-      break;
+    alert('No escribiste un numero');
+
+  } else {
+
+    switch (operador) {
+      case '+':
+        resultado = numeroUno + numeroDos;
+        break;
+      case '-':
+        resultado = numeroUno - numeroDos;
+        break;
+      case '*':
+        resultado = numeroUno * numeroDos;
+        break;
+      case '/':
+        resultado = numeroUno / numeroDos;
+        break;
+      default:
+        alert('No es un operador correcto');
+        break;
+
+    }
   }
 
   alert('El resultado es: ' + resultado);
 
 }
 
-calcular(numeroUno, operador, numeroDos); */
+calcular(numeroUno, operador, numeroDos);
